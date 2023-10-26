678318f49c72f28673d4e3eb107df3666b5095df
laurent <l.marchand@gt-sa.com>
2023-10-26 17:57:15 +0200

    birds

---
+++ assets/app.js
@@ -0,0 +1,17 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import 'bootstrap/dist/css/bootstrap.css'
+import '../assets/css/main.css';
+import '../assets/css/jquery-ui.min.css';
+import '../assets/css/dataTables.bootstrap4.min.css';
+import '../assets/css/main.css';
+import '../assets/css/filter.css';
+import '../assets/css/bootstrap-select.min.css';
+
+import 'bootstrap';
